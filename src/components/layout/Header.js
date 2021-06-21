98024b88d00ebda656dffe57c163f60f46fbba3e
simcothreadmills <85206854+simcothreadmills@users.noreply.github.com>
2021-06-21 18:49:03 +0530

    Update Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,27 +7,21 @@ const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
-        <div className="w-12 mr-3">
-          <LogoIcon />
-        </div>
-        Lander
+        SIMCO THREAD MILLS
       </div>
       <div className="flex mt-4 sm:mt-0">
         <AnchorLink className="px-4" href="#features">
           Features
         </AnchorLink>
-        <AnchorLink className="px-4" href="#services">
+        <AnchorLink className="px-4" href="#products">
           Services
         </AnchorLink>
-        <AnchorLink className="px-4" href="#stats">
+        <AnchorLink className="px-4" href="#aboutus">
           Stats
         </AnchorLink>
-        <AnchorLink className="px-4" href="#testimonials">
+        <AnchorLink className="px-4" href="#contactus">
           Testimonials
         </AnchorLink>
-      </div>
-      <div className="hidden md:block">
-        <Button className="text-sm">Start Free Trial</Button>
       </div>
     </div>
   </header>
